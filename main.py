deba9a6f99dead92944f634fa641bc90d7ce2182
madiyar_9802 <madiarashkenovm@gmail.com>
2024-05-29 23:45:19 +0500

    Updated authentication method, routes, and added endpoints for fetching all tasks and updating a task

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import models
-from config import *
+from config import user, password, host, dbname
 from flask import Flask, jsonify, request
 from basic_auth import requires_auth
-from models import Executor, db
 from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
@@ -18,8 +17,8 @@ def application_check():
     return "200"
 
 
-@app.route('/register', methods=['POST'])
-def register():
+@app.route('/sign_up', methods=['POST'])
+def sign_up():
     data = request.json
     name = data.get('name')
     surname = data.get('surname')
@@ -27,13 +26,13 @@ def register():
     login = data.get('login')
     password = data.get('password')
     if not (name and surname and email and login and password):
-        return jsonify({'error': 'Требуется логин и пароль'}), 400
-    if Executor.query.filter_by(login=login).first():
+        return jsonify({'error': 'Требуется ввести все данные'}), 400
+    if models.Executor.query.filter_by(login=login).first():
         return jsonify({'error': 'Пользователь уже существует'}), 400
     hashed_password = generate_password_hash(password, method='pbkdf2:sha256')
-    new_user = Executor(name=name, surname=surname, email=email, login=login, password=hashed_password)
-    db.session.add(new_user)
-    db.session.commit()
+    new_user = models.Executor(name=name, surname=surname, email=email, login=login, password=hashed_password)
+    models.db.session.add(new_user)
+    models.db.session.commit()
     return jsonify({'message': 'Регистрация успешно завершена'}), 201
 
 
@@ -45,67 +44,41 @@ def change_password():
     new_password = data.get('new_password')
     if not old_password or not new_password:
         return jsonify({'error': 'Требуется ввести старый и новый пароль'}), 400
-    user = Executor.query.filter_by(login=auth.username).first()
-    if not (user or check_password_hash(user.password, old_password)):
+    user = models.Executor.query.filter_by(login=auth.username).first()
+    if not user or not check_password_hash(user.password, old_password):
         return jsonify({'error': 'Старый пароль введен неверно'}), 400
     user.password = generate_password_hash(new_password, method='pbkdf2:sha256')
-    db.session.commit()
+    models.db.session.commit()
     return jsonify({'message': 'Пароль успешно сменен'})
 
 
-@app.route('/project=<int:project_id>')
+# PROJECT METHODS
+@app.route('/get_projects')
 @requires_auth
-def get_project(project_id):
-    project = models.Project.query.get(project_id)
-    if not project:
-        return jsonify({'error': 'Project not found'}), 404
+def get_projects():
+    auth = request.authorization
+    executor = models.Executor.query.filter_by(login=auth.username).first()
+    tasks = models.Task.query.filter_by(executor_id=executor.id).all()
+    if not tasks:
+        return jsonify({'error': 'У пользователя нет задач и проектов'}), 404
 
-    project_data = {
-        'id': project.id,
-        'name': project.name,
-        'description': project.description,
-        'start_time': project.start_time.strftime('%Y-%m-%d %H:%M:%S'),
-        'end_time': project.end_time.strftime('%Y-%m-%d %H:%M:%S')
-    }
-    return jsonify(project_data)
+    projects_data = []
+    for task in tasks:
+        project = models.Project.query.get(task.project_id)
+        if project:
+            project_data = {
+                'id': project.id,
+                'name': project.name,
+                'description': project.description,
+                'start_time': project.start_time.strftime('%Y-%m-%d %H:%M:%S'),
+                'end_time': project.end_time.strftime('%Y-%m-%d %H:%M:%S')
+            }
+            projects_data.append(project_data)
 
-
-@app.route('/project=<int:project_id>', methods=['PUT'])
-@requires_auth
-def update_project(project_id):
-    project = models.Project.query.get(project_id)
-    if not project:
-        return jsonify({'error': 'Project not found'}), 404
-    data = request.json
-    project.name = data.get('name', project.name)
-    project.description = data.get('description', project.description)
-    project.start_time = data.get('start_time', project.start_time)
-    project.end_time = data.get('end_time', project.end_time)
-    models.db.session.commit()
-    return jsonify({'message': 'Project updated successfully'})
+    return jsonify(projects_data)
 
 
-@app.route('/project=<int:project_id>/task=<int:task_id>')
-@requires_auth
-def get_task(project_id, task_id):
-    task = models.Task.query.join(models.Project).filter(models.Task.id == task_id,
-                                                         models.Project.id == project_id).first()
-    if not task:
-        return jsonify({'error': 'Task not found'}), 404
-
-    task_data = {
-        'id': task.id,
-        'project_id': task.project_id,
-        'description': task.description,
-        'start_time': task.start_time.strftime('%Y-%m-%d %H:%M:%S'),
-        'end_time': task.end_time.strftime('%Y-%m-%d %H:%M:%S'),
-        'executor_id': task.executor_id,
-        'status_id': task.status_id
-    }
-    return jsonify(task_data)
-
-
-@app.route('/projects', methods=['POST'])
+@app.route('/create_project', methods=['POST'])
 @requires_auth
 def create_project():
     data = request.json
@@ -120,21 +93,81 @@ def create_project():
     return jsonify({'message': 'Project created successfully'}), 201
 
 
-@app.route('/projects/tasks', methods=['POST'])
+@app.route('/update_project/<int:project_id>', methods=['PUT'])
+@requires_auth
+def update_project(project_id):
+    project = models.Project.query.get(project_id)
+    if not project:
+        return jsonify({'error': 'Project not found'}), 404
+    data = request.json
+    project.name = data.get('name', project.name)
+    project.description = data.get('description', project.description)
+    project.start_time = data.get('start_time', project.start_time)
+    project.end_time = data.get('end_time', project.end_time)
+    models.db.session.commit()
+    return jsonify({'message': 'Project updated successfully'})
+
+
+# TASK METHODS
+@app.route('/get_tasks')
+@requires_auth
+def get_task():
+    auth = request.authorization
+    executor = models.Executor.query.filter_by(login=auth.username).first()
+    tasks = models.Task.query.filter_by(executor_id=executor.id).all()
+    if not tasks:
+        return jsonify({'error': 'У пользователя нет задач'}), 404
+    tasks_data = []
+    for task in tasks:
+        task_data = {
+            'id': task.id,
+            'project_id': task.project_id,
+            'description': task.description,
+            'start_time': task.start_time.strftime('%Y-%m-%d %H:%M:%S'),
+            'end_time': task.end_time.strftime('%Y-%m-%d %H:%M:%S'),
+            'executor_id': task.executor_id,
+            'status_id': task.status_id
+        }
+        tasks_data.append(task_data)
+    return jsonify(tasks_data)
+
+
+@app.route('/create_task', methods=['POST'])
 @requires_auth
 def create_task():
+    auth = request.authorization
     data = request.json
+    executor = models.Executor.query.filter_by(login=auth.username).first()
     new_task = models.Task(
         project_id=data['project_id'],
         description=data['description'],
         start_time=data['start_time'],
         end_time=data['end_time'],
-        executor_id=data['executor_id'],
+        executor_id=executor.id,
         status_id=data['status_id']
     )
     models.db.session.add(new_task)
     models.db.session.commit()
-    return jsonify({'message': 'Task created successfully'}), 201
+    return jsonify({'message': 'Задача успешно создана'}), 201
+
+
+@app.route('/update_task/<int:task_id>', methods=['PUT'])
+@requires_auth
+def update_task(task_id):
+    auth = request.authorization
+    executor = models.Executor.query.filter_by(login=auth.username).first()
+    task = models.Task.query.filter_by(executor_id=executor.id).first()
+    print(task.id)
+    if not task_id == task.id:
+        return jsonify({'error': 'Задача не найдена'}), 404
+    data = request.json
+    task.status_id = data.get('status_id', task.status_id)
+    task.description = data.get('description', task.description)
+    task.start_time = data.get('start_time', task.start_time)
+    task.end_time = data.get('end_time', task.end_time)
+    task.executor_id = data.get('executor_id', task.executor_id)
+    models.db.session.commit()
+    return jsonify({'message': 'Задача успешно обновлена'})
 
 
 if __name__ == '__main__':
