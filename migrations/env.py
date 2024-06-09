6bd1c4882655cfc934e92ceef44479dccebcf34d
madiyar_9802 <madiarashkenovm@gmail.com>
2024-06-09 23:10:30 +0500

    Added error handlers and optimized database queries

--- migrations/env.py
+++ migrations/env.py
@@ -5,6 +5,7 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
+from config.config import  user, password, host, dbname
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -26,6 +27,9 @@ target_metadata = db.metadata
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
 
+# Set the database URL
+database_url = f"postgresql+psycopg2://{user}:{password}@{host}/{dbname}"
+config.set_main_option("sqlalchemy.url", database_url)
 
 def run_migrations_offline() -> None:
     """Run migrations in 'offline' mode.
